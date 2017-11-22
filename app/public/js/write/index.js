49b54c0e7f056110af0f4ff5b725a4df01020b2b
zengting <zengting@maicai360.cn>
2017-11-22 17:32:32 +0800

    write update

--- app/public/js/write/index.js
+++ app/public/js/write/index.js
@@ -1,4 +1,21 @@
 require ('../../less/write/index.less')
 var COMMON = require('../common/common.js');
-
+$(function(){
+	var INDEX = {
+		init: function(){
+			this.bindEvent();
+		},
+		bindEvent: function(){
+			$('.j-e-btn-select').on('click',function(){
+				$(this).find('.j-e-option-list').show();
+			});
+			$('.j-e-option-list li').each(function(i){
+				$(this).on('click',function(){
+					console.log(i);
+				});
+			});
+		}
+	}
+	INDEX.init();
+})
 
