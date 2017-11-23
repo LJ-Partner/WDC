7e4e3e005467adc898797eb4072b8c0aa887d864
zengting <zengting@maicai360.cn>
2017-11-23 17:32:04 +0800

    write ueditor update

--- app/public/js/write/index.js
+++ app/public/js/write/index.js
@@ -4,18 +4,41 @@ $(function(){
 	var INDEX = {
 		init: function(){
 			this.bindEvent();
+			var ue = UE.getEditor('editor');
 		},
 		bindEvent: function(){
 			$('.j-e-btn-select').on('click',function(){
-				$(this).find('.j-e-option-list').show();
+				$(this).toggleClass('open');
 			});
-			$('.j-e-option-list li').each(function(i){
+			$('.j-e-option-list li').on('click',function(){
+				$('.j-e-btn-select em').text($(this).find('a').text());
+				if($(this).find('a').text() == '转载'){
+					$('.pop-w').show();
+				}
+			});
+			$('.j-e-close').on('click',function(){
+				$('.pop-w').css('display','none');
+			});
+			$(document).mouseup(function(e){  
+				var _con = $('.pop');   // 设置目标区域  
+				if(!_con.is(e.target) && _con.has(e.target).length === 0){ // Mark 1  
+					$('.pop-w').css('display','none');  
+				}  
+			}); 
+			$('.j-e-tag-focus').on('focus',function(){
+				$('.j-e-tag-focus-cnt').show();
+			});
+			$('.j-e-tag-tab li').each(function(i){
 				$(this).on('click',function(){
-					console.log(i);
+					$(this).addClass('active').siblings().removeClass('active');
+					$('.j-e-tag-tab-cnt .tab-item').eq(i).show().siblings().hide();
 				});
 			});
+			// $('.j-e-tag-focus').on('blur',function(){
+			// 	$('.j-e-tag-focus-cnt').hide();
+			// });
 		}
 	}
 	INDEX.init();
-})
+});
 
