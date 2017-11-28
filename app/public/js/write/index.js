db22006f40d1a87a64104fcf1546ee4616020c82
zengting <zengting@maicai360.cn>
2017-11-28 17:33:57 +0800

    update

--- app/public/js/write/index.js
+++ app/public/js/write/index.js
@@ -29,16 +29,72 @@ $(function(){
 				$('.j-e-tag-focus-cnt').show();
 			});
 			$('body').on('click', function(e) {
-				e.preventDefault();
+				if(e.target != $('input[type=checkbox]')[0]){
+					e.preventDefault();
+				}
                 if ($(e.target).parents(".j-e-tag-focus-cnt").length === 0 && $(e.target).parents('.tags-info').length === 0 && !$(e.target).hasClass('tags-info'))
                 return $('.j-e-tag-focus-cnt').hide();
             }),
 			$('.j-e-tag-tab li').each(function(i){
-				$(this).on('click',function(){
+				$('.j-e-tag-tab li').on('click',function(){
 					$(this).addClass('active').siblings().removeClass('active');
 					$('.j-e-tag-tab-cnt .tab-item').eq(i).show().siblings().hide();
+					
 				});
 			});
+			$('.item-list li a').unbind().on('click',function(){
+				var txtvalue = $(this).text();
+				if(txtvalue!=''){
+					if($('.info-item').length < 5){
+						INDEX.addTag($(this));
+					}else{
+						return false;	
+					}
+				}	
+			});
+			$('#license').on('change',function(){
+				INDEX.change();
+			});
+		},
+		addTag: function(obj) {
+			var tag = obj.text();
+			if (tag != '') {
+				var i = 0;
+				$('.info-item').each(function() {
+					if ($(this).text() == tag + '×') {
+						$(this).addClass('tag-warning');
+						setTimeout(function(){
+							$('.tag-warning').removeClass('tag-warning');	
+						},1e3);
+						i++;
+					}
+				})
+				if (i > 0) { //说明有重复
+					return false;
+				}
+				$('.form-field-tags').before('<span class="info-item">' + tag + '<em class="j-e-tag-close">×</em></span>'); //添加标签
+				
+				$('.j-e-tag-focus-cnt').css('left',$('.j-e-tag-focus').position().left);
+				$('.j-e-tag-close').on('click', function() {
+					$(this).parent('.info-item').remove();
+					$('.j-e-tag-focus-cnt').css('left',$('.j-e-tag-focus').position().left);
+				});
+			}
+		},
+		change: function(){
+			var t,i;
+        	t = $('#editorStatus'),
+	        t.html('保存中...').removeClass('hide'),
+	        $('#dropIt').addClass('hide'),
+	        clearTimeout(i),
+	        i = setTimeout(function() {
+	            INDEX.save();
+	        }, 4e3);
+		},
+		save: function(){
+        	var t = $('#editorStatus');
+	        t.html('已保存草稿').removeClass('hide');
+	        $('#dropIt').removeClass('hide');	
 		}
 	}
 	INDEX.init();
